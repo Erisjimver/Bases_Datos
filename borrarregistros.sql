367ba6ff4deeb4f8a01b5baa0ebe09e2224e866a
erisjimver <erisjinver@gmail.com>
2020-10-11 12:47:16 -0500

    Detallando en comentarios

--- borrarregistros.sql
+++ borrarregistros.sql
@@ -1,3 +1,6 @@
+--consulta especficia de datos de la tabla persona
 select cedula,nombre,apellido,fechaa,anio,mes,dia from persona;
 
+--borrar a una persona por su nombre
 delete from persona where nombre='Elen';
+
